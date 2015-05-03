6b0fcaf416a666e63163636feeda8456d5922246
chandanch <chandan.cec107@gmail.com>
2015-05-03 15:14:58 +0530

    Phew that worked

---
+++ threadsample/src/com/example/android/threadsample/ProgressNotifier.java
@@ -0,0 +1,11 @@
+/* Author @ Chandan_ch github: chandanch */
+
+package com.example.android.threadsample;
+
+
+/* Author @ Chandan_ch github: chandanch */
+
+
+public interface ProgressNotifier {
+    public void notifyProgress(String paramString);
+}
